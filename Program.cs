2f8508fd8bc7ca6c632d98a75bb61fb9f3b28b9e
Jegern <severunov@gmail.com>
2023-04-21 17:36:30 +0500

    Added text input check. If inappropriate characters have been entered, a message is displayed with them.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,41 @@
-﻿namespace Maxim_Technology_Practice;
+﻿using System.Text.RegularExpressions;
+
+namespace Maxim_Technology_Practice;
 
 internal static class Program {
 	public static void Main(string[] args) {
 		var inputLine = Console.ReadLine();
-
 		if (inputLine is null) return;
 
-		if (inputLine.Length % 2 == 0) {
-			var middleIndex = inputLine.Length / 2;
-
-			var reversedHalfOfLine1 = ReverseString(inputLine[..middleIndex]);
-			var reversedHalfOfLine2 = ReverseString(inputLine[middleIndex..]);
-
-			Console.WriteLine(reversedHalfOfLine1 + reversedHalfOfLine2);
+		var wrongSymbols = Regex.Matches(inputLine, "[^a-z]");
+		if (wrongSymbols.Count > 0) {
+			var wrongSymbolsString = string.Join(", ", wrongSymbols.Select(m => m.Value));
+			Console.WriteLine("Вы неправильно ввели следующие символы: " + wrongSymbolsString);
 		}
 		else {
-			var reversedLine = ReverseString(inputLine);
-
-			Console.WriteLine(reversedLine + inputLine);
+			Console.WriteLine(LineAlgorithm(inputLine));
 		}
+	}
+    
+	private static string LineAlgorithm(string line) {
+		string resultLine;
+		
+		if (line.Length % 2 == 0) {
+			var middleIndex = line.Length / 2;
+
+			var reversedHalfOfLine1 = ReverseString(line[..middleIndex]);
+			var reversedHalfOfLine2 = ReverseString(line[middleIndex..]);
+
+			resultLine = reversedHalfOfLine1 + reversedHalfOfLine2;
+		}
+		else {
+			var reversedLine = ReverseString(line);
+
+			
+			resultLine = reversedLine + line;
+		}
+
+		return resultLine;
 	}
 
 	private static string ReverseString(string s) => new(s.Reverse().ToArray());
